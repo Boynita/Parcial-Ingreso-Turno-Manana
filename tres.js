338da93d0653dac2f09434ba4566ff74092a0e1e
martin <boynita1995@gmail.com>
2020-08-10 11:09:33 -0300

    examen 10/08

--- tres.js
+++ tres.js
@@ -11,5 +11,70 @@ c)el promedio de personas por viaje,  que viajan en invierno
 */
 function mostrar()
 {
-	alert("tres");
+
+	var nombre;
+	var lugar;
+	var respuesta;
+	var personas;
+	var temporada;
+	var mayorPersonas;
+	var titularMayor;
+	var sumaAcumulada = 0;
+	var contadorInvierno = 0;
+    var promedioInvierno = 0;
+
+
+	do {
+
+
+	do {
+		nombre=prompt("ingrese el nombre del titular");
+	} while (!(isNaN(nombre)));
+
+	do {
+		lugar=prompt("ingrese el lugar bariloche, cataratas o salta");
+	} while (lugar != "bariloche" && lugar != "salta"  && lugar != "cataratas" );
+
+	do {
+		temporada= prompt("ingrese la temporada invierno, otoño, verano, primavera");
+	} while (temporada != "otoño" && temporada != "invierno" && temporada != "primavera" && temporada != "verano" );
+
+	do {
+	personas=prompt("ingrese la cantidad de personas");
+	personas=parseInt(personas);
+	} while (isNaN(personas) || personas <= 0);
+
+	if(lugarMayor < lugar){
+		lugarMayor=lugar;
+
+	}
+
+	if(titularMayor < nombre){
+		titularMayor=nombre;
+		mayorPersonas=personas;
+	}
+
+	if(temporada == "invierno"){
+		sumaPersonas=sumaPersonas+personas
+		contadorInvierno++;
+	}
+
+	respuesta=prompt("desea ingresar denuevo?");
+
+
+   } while (respuesta == "si");
+
+   document.write("el lugar mas elegido es " + lugarMayor + "<br>");
+
+   document.write("el nombre del titular que lleva mas pasajeros es " + titularMayor + "<br>");
+if(temporada == "invierno"){
+
+	promedioInvierno=sumaPersonas/contadorInvierno;
+	promedioInvierno=parseInt(promedioInvierno);
+
+	document.write("el promedio de personas por viajq eu viajan en invierno es " + promedioInvierno);
+
+}
+
+
 }

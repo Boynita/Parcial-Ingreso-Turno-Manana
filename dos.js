21c22b7ed3e367ad20d4aab34c775df11a5208da
martin <boynita1995@gmail.com>
2020-08-10 10:29:19 -0300

    Examen 10/08

--- dos.js
+++ dos.js
@@ -7,5 +7,39 @@ b)la marca del más caro de los líquidos
 c)la marca del más liviano de los sólidos
 */function mostrar()
 {
-  alert("dos");
+  var productos;
+  var respuesta;
+  var pesoDeCompra;
+  var totalDeCompra;
+  var marcaMasCaraLiquidos;
+  var marcaMasLivianaSolidos;
+
+  respuesta="S"
+
+  while(respuesta){
+    productos= prompt("ingrese el producto.");
+    while(productos != "marca" && != "precio" && "peso en kilogramos" && != "solido" || != "liquido");
+    productos= prompt("Error, ingrese el producto correcto.");
+
+    pesoDeCompra= parseInt(prompt("Su peso de compra en total es."))
+    while(isNaN(totalDeCompra)){
+      totalDeCompra= parseInt(prompt("Error, en su total de compra."));
+    }
+    do {
+			marcaMasCaraLiquidos = prompt("Ingrese el producto");
+			while (!(isNaN(marcaMasCaraLiquidos))) {
+				marcaMasCaraLiquidos = prompt("Error, ingrese la marca");
+			}
+    } while (!(isNaN(marcaMasCaraLiquidos)));
+
+    do {
+			marcaMasLivianaSolidos = prompt("Ingrese el producto");
+			while (!(isNaN(marcaMasLivianaSolidos))) {
+				marcaMasLivianaSolidos = prompt("Error, ingrese la marca");
+			}
+    } while (!(isNaN(marcaMasLivianaSolidos)));
+    
+  }//end while
+
+  console.log(marcaMasLivianaSolidos + marcaMasCaraLiquidos + totalDeCompra);
 }
--- uno.js
+++ uno.js
@@ -8,6 +8,57 @@ c)la mujer con más temperatura(si la hay)
 pedir datos por prompt y mostrar por document.write o console.log
 */
 function mostrar()
-{
-	alert("uno");
+{	
+	var nombre;
+	var temperatura;
+	var sexoF;
+	var sexoM;
+	var edades;
+	var contador;
+	var cantidadDePersonasFemeninas;
+	var cantidadDePersonasMasculinas;
+	var cantidadDePersonas;
+	var promedioEnTotal;
+
+	var datos;
+
+	temperatura=39;
+	contador=0;
+	promedioEnTotal=0;
+
+
+
+	do { //VALIDANDO DATOS
+		datos = parseInt(prompt("Ingrese los datos."));	
+		
+		}while (isNaN(datos) || datos < 0) {
+			(dato != "nombre" && != "temperatura" && != "edad");
+			datos =(prompt("Error, ingrese los datos correctos."));
+			contador++;
+			
+		cantidadDePersonasFemeninas= prompt("Ingrese sexo.");	
+		cantidadDePersonasFemeninas= parseInt(cantidadDePersonasFemeninas);
+		cantidadDePersonasMasculinas= prompt("Ingrese el sexo.");
+		cantidadDePersonasMasculinas= parseInt (cantidadDePersonasMasculinas);
+
+		while(cantidadDePersonas<5){
+			cantidadDePersonas= prompt("Error, al ingresar los datos.");
+			cantidadDePersonas= parseInt(cantidadDePersonas);
+			}
+		
+		if(edades==promedioEnTotal){
+
+			promedioEnTotal=edad;
+		}else{
+			temperatura=cantidadDePersonasFemeninas;
+		}
+
+
+	}
+
+	console.log("Datos de las personas ingresadas al Hospital" + "datos del sexo" + "nombre" + "Si alguien tiene la temperatura alta."); 
+
+
+	
 }
+
